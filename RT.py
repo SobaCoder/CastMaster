9ad4d0e6e6ec3d41df21aff314eb8c867d15c9ac
Anwesha Bhattacharya <an.bhattacharya@gmail.com>
2013-04-20 21:10:47 -0500

    reading movie lists
    Need to fix some more bugs

--- RT.py
+++ RT.py
@@ -1,22 +1,49 @@
+#IMPORT ALL THE MODULES
+
 from rottentomatoes import RT
 import json
+import re
+import string
+from collections import defaultdict
 
 
-
-r = RT('c6nttc6uvr4pr6ewr996p6yt').search('Toy Story 3')
-w = r[0]
-critics = w['critics_consensus']
-synopsis = w['synopsis']
-word_list = critics + synopsis
-
-print word_list
+#GLOBAL DATA VARIABLES
+Documents = defaultdict(list)
 
 
 
 
-file = open('RT.txt', 'w')
-file.write(str(r))
-file.close()
+def main():
+
+	
+	file = open('MovieList.txt', 'r')			#FILE CONTAINS MOVIE NAMES
+	Size = 1
+	
+	for name in file:			
+
+		#print name
+		r = RT('c6nttc6uvr4pr6ewr996p6yt').search(name)		#GET INFOR FOR EVERY MOVIE USING API KEY
+		w = r[0]
+		if 'critics_consensus' in w.keys():					#GET CRITICS REVIEW
+			critics = w['critics_consensus']		
+		if 'synopsis' in w.keys():							#GET SYNOPSIS
+			synopsis = w.get['synopsis']
+		word_list = critics + synopsis
+
+		W_out = re.sub('[%s]' % re.escape('!@#$%^&*(){},.:;"~`+=|\?/<>-_'), ' ', word_list)	#REMOVE PUNCTUATION - CANNOT REMOVE THE 'U' 
+		W_list = [w.lower() for w in W_out.split()]											#CASE FOLDING AND SPLIT ON WHITE SPACE - TRANSLATE NOT WORKING!!
+		#print W_list
+		Documents[Size] = W_list															#READ INTO CORPUS
+		Size += 1
+	
+	
+	print Documents[20]
+	
+if __name__ == "__main__":
+   main()
+
+
+
 
 
 
